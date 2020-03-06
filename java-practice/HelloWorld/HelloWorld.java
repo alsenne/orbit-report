7adf39bbd81845ac4f51dd95410c25e710a45a22
name <email@email.com>
2020-03-05 19:34:12 -0600

    inital push

---
+++ java-practice/HelloWorld/HelloWorld.java
@@ -0,0 +1,10 @@
+package org.launchcode.java.demos;
+
+/**
+ * From "Java Web Development"
+ */
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello, World");
+    }
+}
